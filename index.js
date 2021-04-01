396d0d656c496ed56685f5b0e0a0619de4ab70a8
valdirpn <valdiraires.pn@gmail.com>
2021-03-31 22:02:16 -0300

    aula 03

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const configExpress = require('./config/configExpress')
 const conexao = require('./infra/conexao')
+const Tabelas = require('./infra/Tabelas')
 
-const app = configExpress()
-const port = 3000
 
 conexao.connect(error => {
     if(error) {
         throw error;
     }
-
-    app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
+    
+    Tabelas.init(conexao)
+    
+    app = configExpress()
+    
+    app.listen(3000, () => console.log(`Servidor rodando na porta 3000`))
 })
-
-
